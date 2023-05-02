aa288e4d814ddbc53837c72af09ffb77d26fe848
mukesh891 <mukesh.saha.100@gmail.com>
2023-05-02 16:14:12 +0530

    change done for queue format and dict format

--- pcie_rc_config_pkt.py
+++ pcie_rc_config_pkt.py
@@ -10,24 +10,74 @@ class pcie_seq_rc_config_pkt(pcie_pkg):
     def __init__(self):
         super().__init__()
     def cf0_pkt(self):
-        self.fmt = random.getrandbits(3)
-        self.bdf = random.getrandbits(17)
-        self.cfg_type = random.getrandbits(1)
-        self.first_dw_be = 0b0011
-        self.ep = random.getrandbits(1)
-        self.block = random.getrandbits(1)
-        self.td= random.getrandbits(1)
+        self.fmt                    = random.getrandbits(3)
+        self.bdf                    = random.getrandbits(16)
+        self.type                   = random.getrandbits(5)
+        self.first_dw_be            = 0b0011
+        self.ep                     = random.getrandbits(1)
+        self.block                  = random.getrandbits(1)
+        self.td                     = random.getrandbits(1)
+        self.tc                     = random.getrandbits(3)
+        self.attr0                  = random.getrandbits(2)
+        self.attr1                  = random.getrandbits(1)
+        self.at                     = random.getrandbits(2)
+        self.length                 = random.getrandbits(10)
+
+        self.bus                    = random.getrandbits(8)
+        self.device                 = random.getrandbits(5)
+        self.function               = random.getrandbits(3)
+        self.tag                    = random.getrandbits(8)
+        self.last_dw_be             = random.getrandbits(4)
+        
+        self.addresses              = random.getrandbits(4)
+
 
         
+        ##              DICTIONARY FORMATTING           ##
         #self.packet = [self.bdf, self.conf_type, self.block, self.ep, self.td]
-        pkt_dict["bdf"] = self.bdf 
-        pkt_dict["conf_type"] = self.conf_type 
-        pkt_dict["block"] = self.block 
-        pkt_dict["ep"] = self.ep 
-        pkt_dict["td"] = self.td
-        pkt_dict["fmt"] = self.fmt
-        pkt_dict["first_dw_be"] = self.first_dw_be
+        #pkt_dict["bdf"] = self.bdf 
+        #pkt_dict["conf_type"] = self.conf_type 
+        #pkt_dict["block"] = self.block 
+        #pkt_dict["ep"] = self.ep 
+        #pkt_dict["td"] = self.td
+        #pkt_dict["fmt"] = self.fmt
+        #pkt_dict["first_dw_be"] = self.first_dw_be
+        ##              DICTIONARY FORMATTING           ##
        
+        ##                  QUEUE  FORMATTING           ##
+        pkt_queue.put(format(self.fmt,'03b'))
+        pkt_queue.put(format(self.type,'05b'))
+        pkt_queue.put(hex(self.bdf))
+        pkt_queue.put(format(self.first_dw_be,'04b'))
+        pkt_queue.put(self.ep)
+        pkt_queue.put(self.td)
+        pkt_queue.put(hex(self.register_number))
+        pkt_queue.put(self.block)
+        
+        pkt_queue.put(self.fmt)         
+        pkt_queue.put(self.bdf)         
+        pkt_queue.put(self.type)        
+        pkt_queue.put(self.first_dw_be) 
+        pkt_queue.put(self.ep)          
+        pkt_queue.put(self.block)       
+        pkt_queue.put(self.td)          
+        pkt_queue.put(self.tc)          
+        pkt_queue.put(self.attr0)       
+        pkt_queue.put(self.attr1)       
+        pkt_queue.put(self.at)          
+        pkt_queue.put(self.length)      
+                       
+        pkt_queue.put(self.bus)         
+        pkt_queue.put(self.device)      
+        pkt_queue.put(self.function)    
+        pkt_queue.put(self.tag)         
+        pkt_queue.put(self.last_dw_be)  
+                       
+        pkt_queue.put(self.addresses)   
+
+
+
+
 
                   
 

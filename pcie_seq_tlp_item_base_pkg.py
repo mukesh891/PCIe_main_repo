c10a4538ba0c3e5a4b324613e0d2707a2d870207
mukesh891 <mukesh.saha.100@gmail.com>
2023-05-03 11:59:41 +0530

    initializing default value of signals to 0

--- pcie_seq_tlp_item_base_pkg.py
+++ pcie_seq_tlp_item_base_pkg.py
@@ -10,24 +10,37 @@ from enum import IntEnum
 
 class pcie_pkg:
     def __init__(self, name="", size_in_bytes=0, xwr=0):
-        with open("bdf_file.txt","w") as f:
-            f.write("bdf")
-            self.fmt = random.getrandbits(3)	
-            self.bdf = 10
-            self.conf_type = 0
-            self.first_dw_be = 0b0011
-            self.ep = 0
-            self.block = random.getrandbits(1)
-            self.td = 0
-            #self.command_num = random.getrandbits(32)
-            ############### code for byte conv #############
-            self.name = name
-            self.size_in_bytes = size_in_bytes
-            self.xwr = xwr
-            ################################################
-            f.write(hex(self.bdf))
-            f.write("\n")
-    
+        self.fmt                    = 0
+        ##    BDF format for requester and completer    ##
+        self.requester_id           = 0
+        self.completer_id           = 0
+        ##############################
+        self.type                   = 0
+        self.first_dw_be            = 0
+        self.ep                     = 0 
+        self.td                     = 0
+        self.tc                     = 0
+        self.attr0                  = 0
+        self.attr1                  = 0
+        self.at                     = 0
+        self.length                 = 0
+
+        #self.bus                    =0
+        #elf.device                 = 0
+        #elf.function               = 0
+        self.tag                    = 0
+        self.last_dw_be             = 0
+        
+        self.payload                = 0
+        self.reserve_bit1           = 0
+        self.reserve_bit2           = 0
+        self.reserve_bit3           = 0
+        self.reserve_bit4           = 0
+        self.reserve_bit5           = 0
+        self.th                     = 0
+        
+        self.ext_register_number    = 0
+        self.register_number        = 0    
     
  
 class tlp_fmt(IntEnum):

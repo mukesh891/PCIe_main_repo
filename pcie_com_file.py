8376fd25d3328167f76a2aeacca1e0085dacc476
mukesh891 <mukesh.saha.100@gmail.com>
2023-05-30 19:51:50 +0530

    rc side logs

--- pcie_com_file.py
+++ pcie_com_file.py
@@ -11,6 +11,7 @@ class pcie_config_obj:
         parser.add_argument('--err_pkt_no', type= int , help = 'Total no. of error pkt to be injected', default = 0)
         parser.add_argument('--ep_err_eij', type= int , help = 'A bit value to represent error injection is done from EP completer', default = 0)
         parser.add_argument('--ep_err_pkt_no', type= int , help = 'Total no. of error pkt to be injected in completer', default = 0)
+        parser.add_argument('--test', type= str , help = 'Total no. of error pkt to be injected in completer', default = 'cfg_seq_test')
         
         return parser.parse_args()
 c=pcie_config_obj
@@ -21,6 +22,7 @@ num_pkts= argv.num_pkts
 
 ep_err_eij = argv.ep_err_eij
 ep_err_pkt_no = argv.ep_err_pkt_no 
+test = argv.test
 
 
 pkt_queue = queue.Queue()
---
+++ pcie_rc_base_test.py
@@ -0,0 +1,54 @@
+from pcie_rc_config_pkt import *
+from pcie_rc_mem_seq import *
+from pcie_com_file import *
+from pcie_rc_com_file import *
+from pcie_rc_generated_logs import *
+
+logging.info("ROOT COMPLEX : Compiling pcie_rc_base_seq.py file")
+
+class pcie_rc_base_test:
+    def __init__(self):
+        num_pkts = argv.num_pkts
+        super().__init__()
+        self.which_seq = 0
+    
+    def run(self, test_name):
+        # Dynamically retrieve the method based on the provided test_name
+        method = getattr(self, test_name, None)
+        
+        if method is not None and callable(method):
+            # Call the retrieved method with the necessary arguments
+            method()
+        else:
+            print(f"Method '{test_name}' not found or not callable.")
+
+class cfg_seq_test(pcie_rc_base_test):
+    def run(self, test_name):
+        super().run(test_name)
+
+    def cfg_test(self):
+        logging.info("ROOT COMPLEX: RUNNING \"cfg_seq_test\"")
+        print("---cfg test")
+        mem_seq.run_mem()
+
+    def cfg_with_mem_seq_test(self):
+        logging.info("ROOT COMPLEX: Compiling pcie_rc_base_seq.py file")
+        print("memory test")
+        for i in range(num_pkts):  # as we are sending 128 bits
+            # First 10 packets have to be config type
+            if i > 9:
+                mem_seq.run_mem()
+            else:
+                cfg_seq.run_cfg()
+
+cfg_seq = pcie_rc_config_pkt()
+mem_seq = pcie_rc_mem_seq()
+config_test = cfg_seq_test()
+config_test.run(test)
+
+g_log=pcie_rc_generated_logs()
+g_log.bin_file_handle()
+g_log.hex_file_handle()
+g_log.gen_log()
+g_log.mem_rd_file_handle()
+
